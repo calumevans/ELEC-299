680af81b02da9ff40ba99d18e2976d3844db9cf6
Calum Evans <38733561+calumevans@users.noreply.github.com>
2019-03-23 20:15:16 -0400

    Update line_sensors.ino

--- Competition/line_sensors.ino
+++ Competition/line_sensors.ino
@@ -1,21 +1,17 @@
 //this code is for checking the values of the line sensors
-
 //----------------------------------ANALOG PINS
 #define sensorL             0
 #define sensorR             1
 #define sensorC             2
 
-
-
 void setup(){
-Serial.begin(9600);
+  Serial.begin(9600);
 }
-
 void loop(){
-Serial.print("Left/Centre/Right:  ");
-      Serial.print(analogRead(sensorL));
-      Serial.print(", ");
-      Serial.print(analogRead(sensorC));
-      Serial.print(", ");
-      Serial.println(analogRead(sensorR));
+  Serial.print("Left/Centre/Right:  ");
+  Serial.print(analogRead(sensorL));
+  Serial.print(", ");
+  Serial.print(analogRead(sensorC));
+  Serial.print(", ");
+  Serial.println(analogRead(sensorR));
 }
